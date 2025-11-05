4080f254aa01648e04fef8184bb7b4d1ff59cbb0
Mateo-Ortiz007 <elsenadordelbanco34@gmail.com>
2025-11-05 17:15:14 -0600

    arreglos 60

--- frontend/src/modules/productos/productos.jsx
+++ frontend/src/modules/productos/productos.jsx
@@ -111,131 +111,127 @@ function Productos() {
         );
 
   return (
-    <div className="main-products">
-      <div className="products-container">
-        <h1>Productos</h1>
+    <div className="products-container">
+      <h1>Productos</h1>
 
-        <input
-          type="text"
-          value={newNombre}
-          placeholder="Ingrese el nombre del nuevo producto"
-          onChange={(e) => setNewNombre(e.target.value)}
-        />
+      <input
+        type="text"
+        value={newNombre}
+        placeholder="Ingrese el nombre del nuevo producto"
+        onChange={(e) => setNewNombre(e.target.value)}
+      />
 
-        <input
-          type="text"
-          value={newtipo}
-          placeholder="Ingrese el tipo del nuevo producto"
-          onChange={(e) => setNewTipo(e.target.value)}
-        />
+      <input
+        type="text"
+        value={newtipo}
+        placeholder="Ingrese el tipo del nuevo producto"
+        onChange={(e) => setNewTipo(e.target.value)}
+      />
 
-        <input
-          type="date"
-          value={newFecha}
-          placeholder="Ingrese la fecha de caducidad del nuevo producto"
-          onChange={(e) => setNewFecha(e.target.value)}
-        />
+      <input
+        type="date"
+        value={newFecha}
+        placeholder="Ingrese la fecha de caducidad del nuevo producto"
+        onChange={(e) => setNewFecha(e.target.value)}
+      />
 
-        <input
-          type="number"
-          value={newprecio}
-          placeholder="Ingrese el precio del producto"
-          onChange={(e) => setNewPrecio(e.target.value)}
-        />
+      <input
+        type="number"
+        value={newprecio}
+        placeholder="Ingrese el precio del producto"
+        onChange={(e) => setNewPrecio(e.target.value)}
+      />
 
-        <button onClick={addProduct}>Agregar</button>
+      <button onClick={addProduct}>Agregar</button>
 
-        {editedModalOpen && (
-          <div className="modal">
-            <div className="modal-content">
-              <h2>Editar producto</h2>
-              <input
-                type="text"
-                value={editednombre}
-                onChange={(e) => setEditedNombre(e.target.value)}
-              />
-              <input
-                type="text"
-                value={editedTipo}
-                onChange={(e) => setEditedTipo(e.target.value)}
-              />
-              <input
-                type="date"
-                value={editedFecha}
-                onChange={(e) => setEditedFecha(e.target.value)}
-              />
-              <input
-                type="number"
-                value={editedPrecio}
-                onChange={(e) => setEditedPrecio(e.target.value)}
-              />
-              <button onClick={saveEdit}>Guardar</button>
-              <button onClick={() => setEditedModalOpen(false)}>
-                Cancelar
-              </button>
-            </div>
+      {editedModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Editar producto</h2>
+            <input
+              type="text"
+              value={editednombre}
+              onChange={(e) => setEditedNombre(e.target.value)}
+            />
+            <input
+              type="text"
+              value={editedTipo}
+              onChange={(e) => setEditedTipo(e.target.value)}
+            />
+            <input
+              type="date"
+              value={editedFecha}
+              onChange={(e) => setEditedFecha(e.target.value)}
+            />
+            <input
+              type="number"
+              value={editedPrecio}
+              onChange={(e) => setEditedPrecio(e.target.value)}
+            />
+            <button onClick={saveEdit}>Guardar</button>
+            <button onClick={() => setEditedModalOpen(false)}>Cancelar</button>
           </div>
-        )}
-
-        {deleteModalOpen && (
-          <div className="modal">
-            <div className="modal-content">
-              <h2>¿Eliminar producto?</h2>
-              <p>¿Estás seguro de eliminar "{productToDelete?.nombre}"?</p>
-              <button onClick={deleteProduct}>Sí, eliminar</button>
-              <button onClick={() => setDeleteModalOpen(false)}>
-                Cancelar
-              </button>
-            </div>
-          </div>
-        )}
-
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "8px",
-            marginBottom: "10px",
-          }}
-        >
-          <FaFilter size={20} />
-          <select
-            onChange={(e) => setFiltrado(e.target.value)}
-            value={filtrado}
-          >
-            <option value="all">Todos</option>
-            {[
-              ...new Set(productos.map((p) => p.tipo)), // tipos únicos
-            ].map((tipo) => (
-              <option key={tipo} value={tipo}>
-                {tipo}
-              </option>
-            ))}
-          </select>
         </div>
+      )}
 
-        <ul>
-          {filterProducts.map((producto) => (
-            <li key={producto.id}>
-              {producto.nombre}{" "}
-              <small style={{ color: "#888", marginLeft: "10px" }}>
-                Fecha de vencimiento: {producto.fecha}
-              </small>
-              <small style={{ color: "#888", marginLeft: "10px" }}>
-                precio:{producto.precio}
-              </small>
-              <button
-                className="lapiz"
-                onClick={() => openEditModal(producto)}
-                style={{ marginLeft: "10px" }}
-              >
-                ✏️
-              </button>
-              <button onClick={() => confirmDeleteProduct(producto)}>❌</button>
-            </li>
+      {deleteModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>¿Eliminar producto?</h2>
+            <p>¿Estás seguro de eliminar "{productToDelete?.nombre}"?</p>
+            <button onClick={deleteProduct}>Sí, eliminar</button>
+            <button onClick={() => setDeleteModalOpen(false)}>Cancelar</button>
+          </div>
+        </div>
+      )}
+
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "8px",
+          marginBottom: "10px",
+        }}
+      >
+        <FaFilter size={20} />
+        <select onChange={(e) => setFiltrado(e.target.value)} value={filtrado}>
+          <option value="all">Todos</option>
+          {[
+            ...new Set(productos.map((p) => p.tipo)), // tipos únicos
+          ].map((tipo) => (
+            <option key={tipo} value={tipo}>
+              {tipo}
+            </option>
           ))}
-        </ul>
+        </select>
       </div>
+
+      <ul>
+        {filterProducts.map((producto) => (
+          <li key={producto.id}>
+            {producto.nombre}{" "}
+            <small style={{ color: "#888", marginLeft: "10px" }}>
+              Fecha de vencimiento: {producto.fecha}
+            </small>
+            <small style={{ color: "#888", marginLeft: "10px" }}>
+              precio:{producto.precio}
+            </small>
+            <button
+              className="lapiz"
+              onClick={() => openEditModal(producto)}
+              style={{ marginLeft: "10px" }}
+            >
+              ✏️
+            </button>
+            <button
+              className="button-delete"
+              onClick={() => confirmDeleteProduct(producto)}
+            >
+              ❌
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
